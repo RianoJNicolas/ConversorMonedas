47b62eb0afc9c4be295364f43fb66f93a3f36b13
RianoJNicolas <rianojnicolas@gmail.com>
2021-09-06 22:17:26 -0500

    Conversion de COP a USD

---
+++ conversor.py
@@ -0,0 +1,9 @@
+pesos = input("Ingrese el valor en COP: ")
+pesos = float(pesos)
+valor_dolar = 3875
+
+dolares = pesos / valor_dolar
+dolares = str(dolares)
+
+print("Tienes $" + dolares +  " dólares")
+ 
