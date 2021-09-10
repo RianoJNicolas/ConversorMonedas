21e41a6a2f814a120af12bcce55492857cb4365c
RianoJNicolas <rianojnicolas@gmail.com>
2021-09-09 21:57:29 -0500

    Agregando conversion ARS y MXN a USD

--- conversor.py
+++ conversor.py
@@ -1,35 +1,49 @@
 menu = """
 Bienvenid@ al conversor de monedas 💰
 
-1 - COP a USD
-2 - ARS a USD
-3 - MXN a USD
+1 - COP (Pesos COlombianos) a USD
+2 - ARS (Pesos Argentinos) a USD
+3 - MXN (Pesos Mexicanos) a USD
 4 - USD a COP
 
 Elige una opción: """
 
 opcion = input(menu)
 
-if opcion=="1":
+if opcion == "1":
     pesos = input("Ingrese el valor en COP: ")
     pesos = float(pesos)
     valor_dolar = 3875
-
     dolares = pesos / valor_dolar
     dolares = round(dolares, 2)
     dolares = str(dolares)
-
     print("Tienes $" + dolares +  " dólares")
 
-elif opcion=="2":
+elif opcion == "2":
+    pesos = input("Ingrese el valor en ARS: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares +  " dólares")
+
+elif opcion == "3":
+    pesos = input("Ingrese el valor en MXN: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares +  " dólares")
+
+elif opcion == "4":
     dolares = input("Ingrese el valor en USD: ")
     dolares = float(dolares)
     valor_dolar = 3875
-
     pesos = dolares * valor_dolar
     pesos = round(pesos, 2)
-    pesos = str(pesos)
-
+    pesos = str(pesos) 
     print("Tienes $" + pesos +  " Pesos")
 
 else:
